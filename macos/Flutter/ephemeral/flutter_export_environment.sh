88b09d41b2b1e3c828bb392202ca545ecf5ee0ae
seiko <seiko_des@outlook.com>
2024-12-09 09:58:31 +0800

    gradle to  8.5 and agp to 8.2.1

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/seiko/.version-fox/cache/flutter/v-3.24.5/flutter-3.24.5"
+export "FLUTTER_APPLICATION_PATH=/Users/seiko/Developer/Android/Test/open-im-flutter-demo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=3.8.1"
+export "FLUTTER_BUILD_NUMBER=31"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
